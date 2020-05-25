82a9f90dd85168373698e0044557af39afa49a6c
ovilladeamigo <ovilladeamigo@ideasoft.uy>
2020-05-25 19:39:37 -0300

    add block

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
-import 'package:peliculas/src/widgets/card_swiper.widget.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal_widget.dart';
+
+import '../models/pelicula_model.dart';
 
 class HomePAge extends StatelessWidget {
   final _peliculaProvider = new PeliculaProvider();
 
   @override
   Widget build(BuildContext context) {
+    _peliculaProvider.getPopulares();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Películas en cine"),
@@ -20,7 +25,8 @@ class HomePAge extends StatelessWidget {
       ),
       body: Container(
         child: Column(
-          children: <Widget>[_swiperTarjetas()],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[_swiperTarjetas(), _footer(context)],
         ),
       ),
     );
@@ -34,11 +40,43 @@ class HomePAge extends StatelessWidget {
             return CardSwiper(peliculas: snapshot.data);
           } else {
             return Container(
-              height: 400.0,
-              child: Center(child: CircularProgressIndicator()));
+                height: 400.0,
+                child: Center(child: CircularProgressIndicator()));
           }
         });
 
     //
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text('Populares',
+                  style: Theme.of(context).textTheme.subtitle2)),
+          SizedBox(
+            height: 5.0,
+          ),
+          StreamBuilder(
+              stream: _peliculaProvider.popularesStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<Pelicula>> snapshot) {
+                if (snapshot.hasData) {
+                  return MovieHorizontal(
+                      peliculas: snapshot.data,
+                      siguientePagina: _peliculaProvider.getPopulares);
+                } else {
+                  return Container(
+                      height: 400.0,
+                      child: Center(child: CircularProgressIndicator()));
+                }
+              })
+        ],
+      ),
+    );
   }
 }
--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -1,17 +1,56 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:peliculas/src/models/pelicula_model.dart';
 import 'package:http/http.dart' as http;
+
+import '../models/pelicula_model.dart';
 
 class PeliculaProvider {
   String _apiKey = 'c67b3e13726cf1b849bc825fc110903f';
   String _url = 'api.themoviedb.org';
   String _language = 'es-UY';
 
+  int _popularesPage = 0;
+
+  List<Pelicula> populares = new List();
+
+  final _popularesStreamController = new StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController?.close();
+  }
+
   Future<List<Pelicula>> getEnCines() async {
     final url = Uri.https(_url, '/3/movie/now_playing',
         {'api_key': _apiKey, 'language': _language});
 
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    _popularesPage++;
+
+    final url = Uri.https(_url, '/3/movie/popular', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': _popularesPage.toString()
+    });
+
+    final resp = await _procesarRespuesta(url);
+
+    populares.addAll(resp);
+
+    popularesSink(populares);
+
+    return resp;
+  }
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
     final decodeData = json.decode(resp.body);
     final peliculas = new Peliculas.fromJsonList(decodeData['results']);
