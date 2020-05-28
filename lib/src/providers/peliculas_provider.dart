2bae7cd94d1123a397cc3b3aed34703bd6de1f2d
ovilladeamigo <ovilladeamigo@ideasoft.uy>
2020-05-28 20:19:23 -0300

    search

--- lib/src/providers/peliculas_provider.dart
+++ lib/src/providers/peliculas_provider.dart
@@ -79,4 +79,11 @@ class PeliculaProvider {
     final actores = new Cast.fromJsonList(decodeData['cast']);
     return actores.items;
   }
+
+  Future<List<Pelicula>> buscarPelicula(String query) async {
+    final url = Uri.https(_url, '/3/search/movie',
+        {'api_key': _apiKey, 'language': _language, 'query': query});
+
+    return await _procesarRespuesta(url);
+  }
 }
